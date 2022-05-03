c19708a86aceaaccbbcafe2aa9b31b3bd917bcc7
Joel Stokes <joel.stokes.e@gmail.com>
2022-05-03 15:35:19 -0400

    Vertical Gen Improvements / Object Pooling

--- Assets/Scripts/Proc Gen/ClassicGen.cs
+++ Assets/Scripts/Proc Gen/ClassicGen.cs
@@ -2,10 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//Procedural Level Generator for the Classic Tower of Death Wheels style (Straight Vertical or Horizontal non-stop until death)
 public class ClassicGen : MonoBehaviour
 {
+    //Path Data for level
     struct ClassicPath{
-        public int prefabNumber;
+        public int objNumber;
         public bool isMirror;
 
         public Vector2 startMin;
@@ -14,54 +16,79 @@ public class ClassicGen : MonoBehaviour
         public Vector2 endMax;
     }
 
-    public bool vertical = true;    //Will be used later to set up Horizontal Classic
+    public bool vertical = true;    //Is the Generator for a Vertical or Horizontal scene
 
+    public GameObject LevelObjectsParent;
     public GameObject[] StarterPrefabs; //Level pieces that are how the area begins
     private ClassicPath StarterPath;
-    public GameObject[] LevelPrefabs;
+    public List<GameObject> LevelObjects = new List<GameObject>();
     private List<ClassicPath> LevelPaths = new List<ClassicPath>();
 
-    public int generationAmount;
-    private int genCount = 0;
+    //private int genCount = 0;
     public int reshuffleRate = 0;   //How soon in generation count to re-shuffle level paths. If too big, will reshuffle when full paths list finished
     private float currentHeight = 0;
     private Vector2 prevMin;
     private Vector2 prevMax;
+    
+    public GameObject Camera;
+    private float genDistance = 15;
 
-    void Start()
+    private int matchingPath = -1;  //Placed here so yielding can occur in while to prevent potential lag
+    //private int pathCount = 0;
+    private bool settingPiece = false;
+    private bool setupComplete = false;
+    public float prePlaceCount = 5;
+
+    void Awake()
     {
+        CollectLevelObjects();
         CollectChunkData();
         AddMirrorPaths();
         ShufflePaths();
         GenerateStarter();
-        GenerateLevel();
 
-        Destroy(gameObject);
+        for(int i=0; i<prePlaceCount; i++){
+            StartCoroutine(PlaceLevelTile());
+        }
+
+        setupComplete = true;
+    }
+
+    void Update(){  //Continuously check & move level chunks depending on Camera position
+        if (Camera.transform.position.y + genDistance >= currentHeight && !settingPiece && setupComplete){
+            StartCoroutine(PlaceLevelTile());
+        }
+    }
+
+    private void CollectLevelObjects(){
+        for (int i=0; i<LevelObjectsParent.transform.childCount; i++){
+            LevelObjects.Add(LevelObjectsParent.transform.GetChild(i).gameObject);
+        }
     }
 
     private void CollectChunkData(){
-        int prefabCounter = 0;
-        foreach(GameObject prefab in LevelPrefabs){ //Grab Start & End locations for every Level Prefab
+        int objCounter = 0;
+        foreach(GameObject obj in LevelObjects){ //Grab Start & End locations for every Level Prefab
             ClassicPath newPath = new ClassicPath();
 
-            newPath.prefabNumber = prefabCounter;
+            newPath.objNumber = objCounter;
             newPath.isMirror = false;
 
-            GameObject StartMinObj = prefab.transform.Find("startMin").gameObject;
-            newPath.startMin = new Vector2(StartMinObj.transform.position.x, StartMinObj.transform.position.y);
+            GameObject StartMinObj = obj.transform.Find("startMin").gameObject;
+            newPath.startMin = new Vector2(StartMinObj.transform.localPosition.x, StartMinObj.transform.localPosition.y);
 
-            GameObject StartMaxObj = prefab.transform.Find("startMax").gameObject;
-            newPath.startMax = new Vector2(StartMaxObj.transform.position.x, StartMaxObj.transform.position.y);
+            GameObject StartMaxObj = obj.transform.Find("startMax").gameObject;
+            newPath.startMax = new Vector2(StartMaxObj.transform.localPosition.x, StartMaxObj.transform.localPosition.y);
 
-            GameObject EndMinObj = prefab.transform.Find("endMin").gameObject;
-            newPath.endMin = new Vector2(EndMinObj.transform.position.x, EndMinObj.transform.position.y);
+            GameObject EndMinObj = obj.transform.Find("endMin").gameObject;
+            newPath.endMin = new Vector2(EndMinObj.transform.localPosition.x, EndMinObj.transform.localPosition.y);
 
-            GameObject EndMaxObj = prefab.transform.Find("endMax").gameObject;
-            newPath.endMax = new Vector2(EndMaxObj.transform.position.x, EndMaxObj.transform.position.y);
+            GameObject EndMaxObj = obj.transform.Find("endMax").gameObject;
+            newPath.endMax = new Vector2(EndMaxObj.transform.localPosition.x, EndMaxObj.transform.localPosition.y);
 
             LevelPaths.Add(newPath);
 
-            prefabCounter++;
+            objCounter++;
         }
     }
 
@@ -75,7 +102,6 @@ public class ClassicGen : MonoBehaviour
     private ClassicPath GetMirrorPath(ClassicPath path){    //Return coordinates of Mirrored Level
         ClassicPath mirroredPath = new ClassicPath();
 
-        mirroredPath.prefabNumber = path.prefabNumber;
         mirroredPath.isMirror = true;
 
         mirroredPath.startMin = new Vector2(-path.startMax.x, path.startMax.y);
@@ -83,68 +109,155 @@ public class ClassicGen : MonoBehaviour
         mirroredPath.endMin = new Vector2(-path.endMax.x, path.endMax.y);
         mirroredPath.endMax = new Vector2(-path.endMin.x, path.endMin.y);
 
+        //This may introduce lag
+        GameObject mirrorObj = GameObject.Instantiate(LevelObjects[path.objNumber], LevelObjectsParent.transform.position, Quaternion.identity);
+        mirrorObj.transform.localScale = new Vector3(-1,1,1);
+        LevelObjects.Add(mirrorObj);
+            
+        mirroredPath.objNumber = LevelObjects.Count-1;
+
         return mirroredPath;
     }
 
     private void ShufflePaths(){    //******CAUSING ERROR! Needs further research
         //LevelPaths = LevelPaths.OrderBy(i => Guid.NewGuid()).ToList();
+
+        //Could have array of GameObjects, delete when under camera?
+
+        for (int i = 0; i < LevelPaths.Count; i++) {
+            ClassicPath temp = LevelPaths[i];
+            int randomIndex = Random.Range(i, LevelPaths.Count);
+            LevelPaths[i] = LevelPaths[randomIndex];
+            LevelPaths[randomIndex] = temp;
+        }
     }
 
     private void GenerateStarter(){
         int rand = Random.Range (0, StarterPrefabs.Length);
 
-        Instantiate(StarterPrefabs[rand], new Vector3(0,0,0), Quaternion.identity);
+        ClassicPath starterPath = new ClassicPath();
+
+        GameObject newStarterChunk = GameObject.Instantiate(StarterPrefabs[rand], new Vector3(0,0,0), Quaternion.identity);
 
         GameObject EndMinObj = StarterPrefabs[rand].transform.Find("endMin").gameObject;
-        prevMin = new Vector2(EndMinObj.transform.position.x, EndMinObj.transform.position.y);
+        starterPath.endMin = new Vector2(EndMinObj.transform.position.x, EndMinObj.transform.position.y);
 
         GameObject EndMaxObj = StarterPrefabs[rand].transform.Find("endMax").gameObject;
-        prevMax = new Vector2(EndMaxObj.transform.position.x, EndMaxObj.transform.position.y);
+        starterPath.endMax = new Vector2(EndMaxObj.transform.position.x, EndMaxObj.transform.position.y);
 
-        currentHeight += prevMin.y;
+        starterPath.startMin = Vector2.zero;
+        starterPath.startMax = Vector2.zero;
+
+        bool mirrorStarter = false;
+        if (Random.Range(0,2) == 1){
+            mirrorStarter = true;
+        }
+
+        if (mirrorStarter){
+            starterPath = GetMirrorPath(starterPath);
+
+            newStarterChunk.transform.localScale = new Vector3(-1,1,1);
+        }
+
+        prevMin = starterPath.endMin;
+        prevMax = starterPath.endMax;
+        currentHeight += prevMin.y + 1;
     }
 
-    private void GenerateLevel(){
-        for (int i = 0; i< generationAmount; i++){
-            int matchingPath = -1;  //Search for path that connects to previous, generate, add height, repeat till gen amount hit
-            int pathCount = 0;
+    /*IEnumerator GenerateLevel(){
+        settingPiece = true;
 
-            Debug.Log("Searching for >= " + prevMin.x + " & <= " + prevMax.x);
+        matchingPath = -1;  //Search for path that connects to previous, generate, add height, repeat till gen amount hit
+        pathCount = 0;
 
-            do{
-                if (LevelPaths[pathCount].startMin.x <= prevMin.x && LevelPaths[pathCount].startMax.x >= prevMax.x){
-                    matchingPath = pathCount;
-                }
-
-                pathCount++;
-                if (pathCount >= LevelPaths.Count-1){
-                    Debug.Log("ERROR! No matches found. Performed " + pathCount + " searches.");
-                    matchingPath = 0;
-                }
-            } while (matchingPath == -1);
-
-            GameObject newLevelChunk = GameObject.Instantiate(LevelPrefabs[LevelPaths[matchingPath].prefabNumber], new Vector3(0,currentHeight,0), Quaternion.identity);
-
-            if (LevelPaths[matchingPath].isMirror){
-                newLevelChunk.transform.localScale = new Vector3(-1,1,1);
+        do{
+            if (LevelPaths[pathCount].startMin.x <= prevMin.x+1 && LevelPaths[pathCount].startMax.x >= prevMax.x-1){
+                matchingPath = pathCount;
             }
 
-            prevMin = LevelPaths[matchingPath].endMin;
-            prevMax = LevelPaths[matchingPath].endMax;
+            pathCount++;
+            if (pathCount >= LevelPaths.Count-1){
+                Debug.Log("ERROR! No matches found. Performed " + pathCount + " searches.");
+                matchingPath = 0;
+            }
 
-            currentHeight += prevMin.y;
-            
-            //Look back into this, could be better way to drop to back of list
-            ClassicPath pathShuffle = LevelPaths[matchingPath];
-            LevelPaths.RemoveAt(matchingPath);
-            LevelPaths.Add(pathShuffle);
+            yield return new WaitForSeconds(.1f);  //Return to loop later to prevent lag
+        } while (matchingPath == -1);
 
-            if (genCount >= reshuffleRate || genCount >= LevelPaths.Count){
-                ShufflePaths();
-                genCount = 0;
-            } else {
-                genCount++;
+        GameObject newLevelChunk = GameObject.Instantiate(LevelPrefabs[LevelPaths[matchingPath].objNumber], new Vector3(0,currentHeight,0), Quaternion.identity);
+
+        if (LevelPaths[matchingPath].isMirror){
+            newLevelChunk.transform.localScale = new Vector3(-1,1,1);
+        }
+
+        prevMin = LevelPaths[matchingPath].endMin;
+        prevMax = LevelPaths[matchingPath].endMax;
+
+        currentHeight += prevMin.y + 1;
+        
+        //Look back into this, could be better way to drop to back of list
+        ClassicPath pathShuffle = LevelPaths[matchingPath];
+        LevelPaths.RemoveAt(matchingPath);
+        LevelPaths.Add(pathShuffle);
+
+        if (genCount >= reshuffleRate || genCount >= LevelPaths.Count){
+            ShufflePaths();
+            genCount = 0;
+        } else {
+            genCount++;
+        }
+
+        settingPiece = false;
+        yield return null;
+    }*/
+
+    IEnumerator PlaceLevelTile(){
+        settingPiece = true;
+
+        matchingPath = -1;  //Search for path that connects to previous, generate, add height, repeat till gen amount hit
+        //pathCount = 0;
+
+        for (int i=0; i<LevelPaths.Count; i++){
+            if (LevelPaths[i].startMin.x <= prevMin.x+1 && LevelPaths[i].startMax.x >= prevMax.x-1){
+                matchingPath = i;
+                break;
             }
         }
+
+        /*do{
+            Debug.Log(pathCount + " - startMin: " + LevelPaths[pathCount].startMin.x + ", startMax: " + LevelPaths[pathCount].startMax.x);
+            if (LevelPaths[pathCount].startMin.x <= prevMin.x+1 && LevelPaths[pathCount].startMax.x >= prevMax.x-1){
+                matchingPath = pathCount;
+            }
+
+            pathCount++;
+            if (pathCount >= LevelPaths.Count-1){
+                Debug.Log("ERROR! No matches found. Performed " + (pathCount+1) + " searches.");
+                matchingPath = 0;
+            }
+
+            yield return 0;  //Return to loop later to prevent lag
+        } while (matchingPath == -1);*/
+
+        LevelObjects[LevelPaths[matchingPath].objNumber].transform.position = new Vector3(0, currentHeight, 0);
+
+        prevMin = LevelPaths[matchingPath].endMin;
+        prevMax = LevelPaths[matchingPath].endMax;
+
+        currentHeight += prevMin.y + 1;
+
+        ClassicPath pathShuffle = LevelPaths[matchingPath];
+        LevelPaths.RemoveAt(matchingPath);
+        LevelPaths.Add(pathShuffle);
+
+        /*if (genCount >= reshuffleRate || genCount >= LevelPaths.Count){
+            ShufflePaths();
+            genCount = 0;
+        } else {
+            genCount++;
+        }*/ //Only shuffling at start now
+
+        settingPiece = false;
+        yield return null;
     }
 }
